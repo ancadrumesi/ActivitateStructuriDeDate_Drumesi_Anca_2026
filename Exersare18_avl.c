acc53b8e7f0be193219da22674efda2a44c38f02
Anca Drumesi <anca.drumesi@sisc.ro>
2026-05-17 13:03:17 +0300

    de finalizat

--- Exersare18_avl.c
+++ Exersare18_avl.c
@@ -3,7 +3,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct 
+typedef struct Serial Serial;
+struct Serial
+{
+	int id;
+	int nrSezoane;
+	char* numeSerial;
+	char* personaj;
+	float rating;
+};
+
+typedef struct NodArbore NodArbore;
+struct NodArbore
+{
+	Serial info;
+	NodArbore* stanga;
+	NodArbore* dreapta;
+};
+
+Serial citireSerialDinFisier(FILE* file)
+{
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Serial s;
+	aux = strtok(buffer, sep);
+	s.id = atoi(aux);
+	aux = strtok(NULL, sep);
+	s.nrSezoane = atoi(aux);
+	aux = strtok(NULL, sep);
+	s.numeSerial = (char*)malloc(strlen(aux) + 1);
+	strcpy_s(s.numeSerial, strlen(aux) + 1, aux);
+	aux = strtok(NULL, sep);
+	s.personaj = (char*)malloc(strlen(aux) + 1);
+	strcpy_s(s.personaj, strlen(aux) + 1, aux);
+	s.rating = atof(strtok(NULL, sep));
+	return s;
+}
+
+void afisareSerial(Serial serial)
+{
+	printf("Id: %d\n", serial.id);
+	printf("Nr sezoane: %d\n", serial.nrSezoane);
+	printf("Nume serial: %s\n", serial.numeSerial);
+	printf("Personaj: %s\n", serial.personaj);
+	printf("Rating: %.2f\n", serial.rating);
+}
+
+int calculeazaInaltimeArbore(NodArbore* radacina)
+{
+	if (radacina == NULL)
+	{
+		return 0;
+	}
+	int inaltimeDreapta = calculeazaInaltimeArbore(radacina->dreapta);
+	int inaltimeStanga = calculeazaInaltimeArbore(radacina->stanga);
+	return 1 + max(inaltimeDreapta, inaltimeStanga);
+}
+
+
 
 int main()
 {
