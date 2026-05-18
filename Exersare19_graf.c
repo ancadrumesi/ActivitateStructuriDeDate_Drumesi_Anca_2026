f2234d7622ca3952ea042efc0c0573f2d250f293
Anca Drumesi <anca.drumesi@sisc.ro>
2026-05-18 17:41:39 +0300

    finalizat

--- Exersare19_graf.c
+++ Exersare19_graf.c
@@ -26,7 +26,7 @@ struct NodPrincipal
 
 typedef struct NodSecundar
 {
-	NodSecundar* info;
+	NodPrincipal* info;
 	NodSecundar* next;
 };
 
@@ -37,7 +37,7 @@ Serial citireSerialDinFisier(FILE* file)
 	fgets(buffer, 100, file);
 	char* aux;
 	Serial s;
-	aux = strtok(sep, buffer);
+	aux = strtok(buffer, sep);
 	s.id = atoi(aux);
 	s.nrSezoane = atoi(strtok(NULL, sep));
 	aux = strtok(NULL, sep);
@@ -111,24 +111,77 @@ NodPrincipal* cautaNodDupaId(NodPrincipal* listaPrincipala, int idCautat)
 	}
 }
 
-////inserare muchie
-//void inserareMuchie(NodPrincipal* listaPrincipala, int idStart, int idStop) {
-//
-//	if (listaPrincipala)
-//	{
-//		NodPrincipal* nodStart = cautaNodDupaID(listaPrincipala, idStart);
-//		NodPrincipal* nodStop = cautaNodDupaID(listaPrincipala, idStart);
-//		if (nodStart && nodStop)
-//		{
-//			inserareListaSecundaraLaInceput(&nodStart->vecini, nodStop);
-//			inserareListaSecundaraLaInceput(&nodStop->vecini, nodStart);
-//		}
-//	}
-//}
-//
+void inserareMuchie(NodPrincipal* listaPrincipala, int idStart, int idStop)
+{
+	if (listaPrincipala)
+	{
+		NodPrincipal* nodStart = cautaNodDupaId(listaPrincipala, idStart);
+		NodPrincipal* nodStop = cautaNodDupaId(listaPrincipala, idStop);
+		if (nodStart && nodStop)
+		{
+			inserareListaSecundaraLaFinal(&nodStart->vecini, nodStop);
+			inserareListaSecundaraLaFinal(&nodStop->vecini, nodStart);
+		}
+	}
+}
+
+NodPrincipal* citireNoduriSerialeDinFisier(const char* numeFisier)
+{
+	FILE* file = fopen(numeFisier, "r");
+	NodPrincipal* listaPrincipala = NULL;
+	if (file)
+	{
+		while (!feof(file))
+		{
+			inserareListaPrincipalaLaFinal(&(listaPrincipala), citireSerialDinFisier(file));
+		}
+	}
+	fclose(file);
+	return listaPrincipala;
+}
+
+void citireMuchiiDinFisier(NodPrincipal* listaPrincipala, const char* numeFisier)
+{
+	FILE* file = fopen(numeFisier, "r");
+	int idStart;
+	int idStop;
+	while (!feof(file))
+	{
+		fscanf(file, "%d %d", &idStart, &idStop);
+		inserareMuchie(listaPrincipala, idStart, idStop);
+	}
+	fclose(file);
+}
+
+void dezalocare(NodPrincipal** graf)
+{
+	while (*graf)
+	{
+		NodPrincipal* p = *graf;
+		NodSecundar* s = p->vecini;
+		while (s)
+		{
+			NodSecundar* auxS = p->vecini;
+			s = s->next;
+			free(auxS);
+		}
+		free(p->info.numeSerial);
+		free(p->info.personaj);
+		*graf = p->next;
+		free(p);
+	}
+}
 
 int main()
 {
+	NodPrincipal* graf = citireNoduriSerialeDinFisier("seriale.txt");
+	citireMuchiiDinFisier(graf, "muchii.txt");
+
+	if (graf) printf("Graful a fost incarcat cu succes!\n");
+
+	dezalocare(&graf);
+	printf("Memoria a fost eliberata.\n");
+	return 0;
 
 	return 0;
 }
